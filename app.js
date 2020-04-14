0a6c9ba9642596282f5d58ee931d89f485fad60a
rodriguezh21 <rodriguezh21@gmail.com>
2020-04-14 16:10:25 -0500

    added gitignore and app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const inquirer = require("inquirer");
+
+const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer")
+const Intern = require("./lib/Intern")
+const Manager = require("./lib/Manager")
+
+function userInput() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What is your name?",
+            name: "name"
+        },
+        {
+            type: "input",
+            message: "What is your Email address?",
+            name: "email"
+        },
+        {
+            type: "input",
+            message: "What is your role?",
+            name: "role",
+            choices: ['Manager', 'Engineer', 'Intern']
+        },
+        {
+            type: "input",
+            message: "Enter your github username:",
+            name: "github"
+        }
+    ])
+    
+}
